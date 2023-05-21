bfbf55dd4e3460cba5b4d65164b83b39ec4c1652
Deiini <K3K3land@gmail.com>
2023-05-21 23:32:47 +0200

    Add files via upload

--- database/connect.py
+++ database/connect.py
@@ -1,12 +1,22 @@
 import sqlite3
 
-c = sqlite3.connect('file:database?mode=rw', uri=True)
-curseur = c.cursor()
 
-consult = ' SELECT * FROM achats '
+def consultation_database():
+    conn = sqlite3.connect('file:database/database?mode=rw', uri=True)
 
-curseur.execute(consult)
-resultat = curseur.fetchall()
+    curseur = conn.cursor()
+    curseur.execute('SELECT * FROM achats')
+    rows = curseur.fetchall()
+    conn.close()
+    return rows
 
-print(resultat)
-c.close()
+
+def enregistrer_achat(matiere_premieres, quantite, prix, ):
+    total = (int(quantite)) * (int(prix))
+
+    conn = sqlite3.connect('file:database/database?mode=rw', uri=True)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO achats (matiere_premieres, quantite, prix, total) VALUES (?, ?, ?, ?)',
+                   (matiere_premieres, quantite, prix, total))
+    conn.commit()
+    conn.close()
